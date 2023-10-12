0638adfe5fb7210346bc772aa49fcbb9031ccdad
SabehShaikh <sabehshaikh201@gmail.com>
2023-10-12 16:19:38 +0500

    completed class2 code.

--- 10-10-2023/main.py
+++ 10-10-2023/main.py
@@ -4,3 +4,20 @@ list = [12 , 'babar' , 'azam' , 'osama']
 print(list[0:3])
 print(list[:3])
 print(list[2:])
+
+#change value
+list[2] = 'rizwan'
+print(list)
+
+#more than one value
+list[2:2] = ['azam']
+print(list)
+
+#insert function
+list.insert(1, 13)
+print(list)
+
+#extend function
+list2 = [7,2]
+list.extend(list2)
+print(list)
