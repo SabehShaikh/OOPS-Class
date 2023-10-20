fc928060aa2f45d127f2b5be24307b8b1d87e25d
SabehShaikh <sabehshaikh201@gmail.com>
2023-10-20 23:51:53 +0500

    17 oct half class code.

---
+++ 17-10-2023/main.py
@@ -0,0 +1,27 @@
+# Create a tuple containing four strings---> cannot be modified.
+tuple = ("sabeh", "sabeh", "shaikh", "shaikh")
+
+# Create a set containing four strings--> duplicates are not allowed.
+set = {"sabeh", "pakistan", "babar", "rizwan",}
+
+# Create a list containing three strings--> can be modified.
+list = ["ali", "raza", "pak"]
+
+# Print the type of the 'tuple' variable, which will be <class 'tuple'>
+print(type(tuple))
+
+# Print the type of the 'set' variable, which will be <class 'set'>
+print(type(set))
+
+# Print the type of the 'list' variable, which will be <class 'list'>
+print(type(list))
+
+# Print the contents of the 'tuple' variable, which will include all four elements
+print(tuple)
+
+# Print the contents of the 'set' variable, which may not preserve the order of elements
+print(set)
+
+# Print the contents of the 'list' variable, which will include all three elements
+print(list)
+
