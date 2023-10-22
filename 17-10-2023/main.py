636f875f8a6fb1423b1e670c33e78ad1ff572e74
SabehShaikh <sabehshaikh201@gmail.com>
2023-10-22 23:54:25 +0500

    completed 17oct class code.

--- 17-10-2023/main.py
+++ 17-10-2023/main.py
@@ -1,27 +1,50 @@
 # Create a tuple containing four strings---> cannot be modified.
-tuple = ("sabeh", "sabeh", "shaikh", "shaikh")
+my_tuple = ("sabeh", "sabeh", "shaikh", "shaikh")
 
 # Create a set containing four strings--> duplicates are not allowed.
-set = {"sabeh", "pakistan", "babar", "rizwan",}
+my_set = {"sabeh", "pakistan", "babar", "rizwan"}
 
 # Create a list containing three strings--> can be modified.
-list = ["ali", "raza", "pak"]
+my_list = ["ali", "raza", "pak"]
 
-# Print the type of the 'tuple' variable, which will be <class 'tuple'>
-print(type(tuple))
+# Print the type of the 'my_tuple' variable, which will be <class 'tuple'>
+print(type(my_tuple))
 
-# Print the type of the 'set' variable, which will be <class 'set'>
-print(type(set))
+# Print the type of the 'my_set' variable, which will be <class 'set'>
+print(type(my_set))
 
-# Print the type of the 'list' variable, which will be <class 'list'>
-print(type(list))
+# Print the type of the 'my_list' variable, which will be <class 'list'>
+print(type(my_list))
 
-# Print the contents of the 'tuple' variable, which will include all four elements
-print(tuple)
+# Print the contents of the 'my_tuple' variable, which will include all four elements
+print(my_tuple)
 
-# Print the contents of the 'set' variable, which may not preserve the order of elements
-print(set)
+# Print the contents of the 'my_set' variable, which may not preserve the order of elements
+print(my_set)
 
-# Print the contents of the 'list' variable, which will include all three elements
-print(list)
+# Print the contents of the 'my_list' variable, which will include all three elements
+print(my_list)
 
+# Conditional statements
+# Define the scores for three subjects: English, Math, and PST.
+english = 45
+maths = 80
+pst = 50
+
+# Check if the score in English is greater than or equal to 40.
+if english >= 40:
+    print('You have passed in English')  # Print a message indicating the person passed in English.
+else:
+    print('You have failed in English')  # Print a message indicating the person failed in English.
+
+# Check if the score in Math is greater than or equal to 40.
+if maths >= 40:
+    print('You have passed in Math')  # Print a message indicating the person passed in Math.
+else:
+    print('You have failed in Math')  # Print a message indicating the person failed in Math.
+
+# Check if the score in PST is greater than or equal to 40.
+if pst >= 40:
+    print('You have passed in PST')  # Print a message indicating the person passed in PST.
+else:
+    print('You have failed in PST')  # Print a message indicating the person failed in PST.
