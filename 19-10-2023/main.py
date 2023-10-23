0704fd962cc6695d13bb4aad8ac907a793747439
SabehShaikh <sabehshaikh201@gmail.com>
2023-10-24 00:24:54 +0500

    added dictionary.

--- 19-10-2023/main.py
+++ 19-10-2023/main.py
@@ -24,3 +24,53 @@ elif operator == '/':
     print(div)
 else:
     print('Invalid operator')
+
+# Create a dictionary named thisdict with three key-value pairs
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+# Print the value associated with the "brand" key using the get() method
+print(thisdict.get("brand"))
+
+# Print the entire dictionary
+print(thisdict)
+
+# Access and print the value associated with the "brand" key directly
+print(thisdict["brand"])
+
+# Print the type of the variable thisdict (which is a dictionary)
+print(type(thisdict))
+
+# Print the keys of the dictionary using the keys() method
+print(thisdict.keys())
+
+# Print the values of the dictionary using the values() method
+print(thisdict.values())
+
+# Create a dictionary named dict with three key-value pairs
+dict = dict(name="John", age=36, country="Norway")
+
+# Print the entire dictionary
+print(dict)
+
+# Create a dictionary named car with three key-value pairs
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+# Get a view of the keys in the car dictionary
+x = car.keys()
+
+# Print the keys view before making changes to the dictionary
+print(x)  # before the change
+
+# Add a new key-value pair ("color": "white") to the car dictionary
+car["color"] = "white"
+
+# Print the keys view after making changes to the dictionary
+print(x)  # after the change
