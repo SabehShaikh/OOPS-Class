470efc38af65f363c09b760aac8a4967bcf26ee3
SabehShaikh <sabehshaikh201@gmail.com>
2023-11-03 15:57:21 +0500

    add 31Oct Class Code.

---
+++ 31-10-2023/main.py
@@ -0,0 +1,36 @@
+# Types of Loops:
+# For Loop
+# While Loop
+# Do while Loop
+
+# Example of a for loop that prints "Free Palestine" 5 times
+for i in range(5):
+    print("Free Palestine")
+
+# Example of a for loop that prints numbers from 1 to 19
+for i in range(1, 20):
+    print(i)
+
+# Taking user input and using it in a for loop to print a number 4 times
+number = int(input('Enter the number you want: '))
+for a in range(4):
+    print(number)
+
+# Example of a for loop that prints even numbers from 10 to 20
+for x in range(10, 21, 2):
+    print(x)
+
+# Iterating through a list of subjects and printing each one
+Subject = ['English', 'Maths', 'Physics']
+for i in Subject:
+    print(i)
+
+# Iterating through a tuple of numbers and printing each one
+num = (1, 2, 3, 4, 5, 6)
+for i in num:
+    print(i)
+
+# Iterating through a set of player names and printing each one
+player = {'Babar', 'Azam'}
+for i in player:
+    print(i)
